b3af5d9bb33851c1dcb44527235820cf0c2edb29
Abner <alejandro-perry2011@hotmail.com>
2023-05-17 16:40:55 -0600

    tarjeta layout de vista opciones

--- app/src/main/java/com/example/parkin/ADondeVamos.kt
+++ app/src/main/java/com/example/parkin/ADondeVamos.kt
@@ -1,11 +1,21 @@
 package com.example.parkin
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.activity.ComponentActivity
 import android.os.Bundle
+import android.widget.Button
 
 class ADondeVamos : ComponentActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_adonde_vamos)
+
+        val btnBuscar = findViewById<Button>(R.id.btn_buscar)
+        btnBuscar.setOnClickListener {
+            val intent = Intent(this, VistaOpciones::class.java)
+            startActivity(intent)
+        }
     }
 }
---
+++ app/src/main/java/com/example/parkin/VistaOpciones.kt
@@ -0,0 +1,11 @@
+package com.example.parkin
+
+import androidx.activity.ComponentActivity
+import android.os.Bundle
+
+class VistaOpciones : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_vista_opciones)
+    }
+}
