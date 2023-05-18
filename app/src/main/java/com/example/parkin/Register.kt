6cc9ab2be3a900f241a190caec618223eff3d733
Abner <alejandro-perry2011@hotmail.com>
2023-05-18 16:49:12 -0600

    Mis spots Layout

--- app/src/main/java/com/example/parkin/Register.kt
+++ app/src/main/java/com/example/parkin/Register.kt
@@ -1,7 +1,9 @@
 package com.example.parkin
 
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.ComponentActivity
 
 
@@ -9,5 +11,12 @@ class Register : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+        val btnRegistrarme= findViewById<Button>(R.id.btn_registrarme)
+        btnRegistrarme.setOnClickListener {
+            val intent = Intent(this, MisSpots::class.java)
+            startActivity(intent)
+        }
+
     }
 }
