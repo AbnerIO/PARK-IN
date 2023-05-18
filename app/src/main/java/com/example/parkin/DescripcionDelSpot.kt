a02f21a79dd15d379cadef2ce517071c00fe1cbb
Abner <alejandro-perry2011@hotmail.com>
2023-05-18 17:14:15 -0600

    Descripcion del spot Layout

---
+++ app/src/main/java/com/example/parkin/DescripcionDelSpot.kt
@@ -0,0 +1,16 @@
+package com.example.parkin
+
+import android.content.Intent
+import androidx.activity.ComponentActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+
+class DescripcionDelSpot : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_descripcion_del_spot)
+
+
+    }
+}
--- app/src/main/java/com/example/parkin/MisSpots.kt
+++ app/src/main/java/com/example/parkin/MisSpots.kt
@@ -1,11 +1,19 @@
 package com.example.parkin
 
+import android.content.Intent
 import androidx.activity.ComponentActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MisSpots : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_mis_spots)
+
+        val btnEditarSpot= findViewById<Button>(R.id.btn_editarSpot)
+        btnEditarSpot.setOnClickListener {
+            val intent = Intent(this, DescripcionDelSpot::class.java)
+            startActivity(intent)
+        }
     }
 }
