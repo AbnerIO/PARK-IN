256a05b9c377efcb44df0e876be488ca43406a6a
Abner <alejandro-perry2011@hotmail.com>
2023-05-20 18:19:58 -0600

    Perfil Propietario

---
+++ app/src/main/java/com/example/parkin/MiPerfilPropietario.kt
@@ -0,0 +1,12 @@
+package com.example.parkin
+
+import androidx.activity.ComponentActivity
+import android.os.Bundle
+
+class MiPerfilPropietario : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_mi_perfil_propietario)
+
+    }
+}
--- app/src/main/java/com/example/parkin/MisSpots.kt
+++ app/src/main/java/com/example/parkin/MisSpots.kt
@@ -11,16 +11,24 @@ class MisSpots : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_mis_spots)
 
+        // Funciones base
+        val btnTicketsPropietarios= findViewById<ImageView>(R.id.btn_ticketsPropietario)
+        btnTicketsPropietarios.setOnClickListener {
+            val intent = Intent(this, TicketsPropietarios::class.java)
+            startActivity(intent)
+        }
+        val btnPerfilPropietario= findViewById<ImageView>(R.id.btn_Profile)
+        btnPerfilPropietario.setOnClickListener {
+            val intent = Intent(this, MiPerfilPropietario::class.java)
+            startActivity(intent)
+        }
+
         val btnEditarSpot= findViewById<Button>(R.id.btn_editarSpot)
         btnEditarSpot.setOnClickListener {
             val intent = Intent(this, DescripcionDelSpot::class.java)
             startActivity(intent)
         }
 
-        val btnTicketsPropietarios= findViewById<ImageView>(R.id.btn_ticketsPropietario)
-        btnTicketsPropietarios.setOnClickListener {
-            val intent = Intent(this, TicketsPropietarios::class.java)
-            startActivity(intent)
-        }
+
     }
 }
